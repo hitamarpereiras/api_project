1b26c2eaab219761777b9c64ee6d0d4de2a807a8
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2025-12-19 15:14:39 -0300

    15º products/views atualizado

--- products/views.py
+++ products/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Produto
+from .serializers import ProdutoSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class  ProdutoListView(generics.ListAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['categoria', 'promocao']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        categoria = self.request.query_params.get('categoria')
+        promocao = self.request.query_params.get('promocao')
+
+        if categoria:
+            queryset = queryset.filter(categoria__iexact=categoria.lower())
+
+        if promocao is not None:
+            promocao_bool = promocao.lower() == 'true'
+            queryset = queryset.filter(promocao=promocao_bool)
+
+        return queryset
+    
+    def list(self, request, *args, **kwargs):
+
+        queryset = self.get_queryset()
+
+        if not queryset.exists():
+            categoria = request.query_params.get('categoria')
+            if categoria:
+                mensagem = f"Produto encontrado na categoria '{categoria}'."
+            else:
+                mensagem = "Nenhum produto encontrado."
+            return Response({"mensagem": mensagem}, status=status.HTTP_200_OK)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+
+# Metodo Post
+class ProdutoCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = ProdutoSerializer(data=request.data)
+
+        if serializer.is_valid():
+            product = serializer.save()
+            return Response(
+                {
+                    "message": "Produto salvo com sucesso!",
+                    "produto": ProdutoSerializer(product).data
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
